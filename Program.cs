80f950d03a27d56e958dea315341c758e80284d5
Skviki <159062869+Skviki@users.noreply.github.com>
2026-07-09 00:23:05 +0300

    1.3.5

--- Program.cs
+++ Program.cs
@@ -22,16 +22,18 @@ void TokenAnalize(string[] tokens)
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Super easy");
             Help("dir                       (Your current directory where you are located)");
+            Help("make_dir |folder name|    (Your current directory where you are located)");
             Help("date                      (Your date now)");
             Help("clear                     (Clear the console)");
             Help("optimize                  (Free up RAM. Not recommended for frequent use)");
             Help("bondarchuk                (Shows you a cute little boy Bondarchuk)");
-            Help("shutdown / shutdown now   (Shut down the OS)");
+            Help("shutdown                  (Shut down the OS)");
             Help("go_to_arch                (The command you can use to exit our C#OS shell.)");
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("Easy");
             Help("say none/good/bad/warring |text|  (Output text to the console)");
             Help("cd |path|                         (Change director)");
+            Help("ls none/|path|                    (Shows what is inside the current folder or the folder chosen by you)");
             Help("run_csfile |path|                 (Reads a text file and executes each line as a system command. Empty lines and comments (with #) are ignored.)");
             Help("run |path| or |ArchLinux command| (Executes the file located at the specified path)");
             Help("system os/pc_name/info            (Display information about the device)");
@@ -63,6 +65,52 @@ void TokenAnalize(string[] tokens)
             if (tokens[1] != "")
                 Warring("The remaining commands have been ignored because this command does not require any arguments. We recommend not wasting your energy on typing extra words ;)");
             break;
+        case "make_dir":
+            Directory.CreateDirectory(tokens[1]);
+            Console.WriteLine(Path.Combine(Directory.GetCurrentDirectory(), tokens[1]));
+            break;
+        case "ls":
+            switch (tokens[1])
+            {
+                case  "":
+                    string[] directories = Directory.GetDirectories(Directory.GetCurrentDirectory());
+                    string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    for (int i = 0; i < directories.Length; i++)
+                    {
+                        Console.WriteLine($"  ^ {directories[i]}");
+                    }
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        Console.WriteLine($"  > {files[i]}");
+                    }
+                    break;
+                default:
+                    try
+                    {
+                        string[] directoriesUser = Directory.GetDirectories(tokens[1]);
+                        string[] filesUser = Directory.GetFiles(tokens[1]);
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        for (int i = 0; i < directoriesUser.Length; i++)
+                        {
+                            Console.WriteLine($"  ^ {directoriesUser[i]}");
+                        }
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        for (int i = 0; i < filesUser.Length; i++)
+                        {
+                            Console.WriteLine($"  > {filesUser[i]}");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Error(e.Message);
+                    }
+                    
+                    break;
+            }
+            
+            break;
         case "cd":
             try
             {
@@ -86,7 +134,8 @@ void TokenAnalize(string[] tokens)
                 case "write":
                     if (tokens[2] == "this")
                     {
-                        File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "file.txt"), string.Join(" ", tokens.Skip(2)));
+                        File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "file.txt"), string.Join(" ", tokens.Skip(3)));
+                        Good("file written" +  Path.Combine(Directory.GetCurrentDirectory(), "file.txt"));
                     }
                     else
                     {
@@ -96,7 +145,7 @@ void TokenAnalize(string[] tokens)
                         }
                         catch (Exception e)
                         {
-                            Error("Fail to write : " + e);
+                            Error("Fail to write : " + e.Message);
                         }
                     }
                     break;
@@ -148,19 +197,27 @@ void TokenAnalize(string[] tokens)
             break;
         case "system":
             switch (tokens[1])
-            {
+            { 
+                    
                 case "os":
                     Console.WriteLine($"Os : {Environment.OSVersion}");
                     break;
                 case "pc_name":
                     Console.WriteLine($"Pc name : {Environment.MachineName}");
                     break;
+                case "disk_space":
+                    DriveInfo diskSpace = new DriveInfo(Environment.CurrentDirectory);
+                    Console.WriteLine($"Disk Space | {(diskSpace.TotalSize - diskSpace.TotalFreeSpace) / 1024 / 1024 / 1024}GB / {diskSpace.TotalSize / 1024 / 1024 / 1024}GB");
+                    Good($"{diskSpace.TotalFreeSpace / 1024 / 1024 / 1024}GB Free Space");
+                    break;
                 case "info":
-                    Console.WriteLine($"OS        | {Environment.OSVersion}");
-                    Console.WriteLine($"PC        | {Environment.MachineName}");
-                    Console.WriteLine($"USER      | {Environment.UserName}");
-                    Console.WriteLine($"CPU_COUNT | {Environment.ProcessorCount}");
-                    Console.WriteLine($"BIT       | {(Environment.Is64BitOperatingSystem ? "64-bit" : "32-bit")}");
+                    DriveInfo driveInfo = new DriveInfo(Environment.CurrentDirectory);
+                    Console.WriteLine($"OS         | {Environment.OSVersion}");
+                    Console.WriteLine($"PC         | {Environment.MachineName}");
+                    Console.WriteLine($"USER       | {Environment.UserName}");
+                    Console.WriteLine($"CPU_COUNT  | {Environment.ProcessorCount}");
+                    Console.WriteLine($"BIT        | {(Environment.Is64BitOperatingSystem ? "64-bit" : "32-bit")}");
+                    Console.WriteLine($"Disk Space | {(driveInfo.TotalSize - driveInfo.TotalFreeSpace) / 1024 / 1024 / 1024}GB / {driveInfo.TotalSize / 1024 / 1024 / 1024}GB , Free Space {driveInfo.TotalFreeSpace / 1024 / 1024 / 1024}GB");
                     break;
                 case "help":
                     Console.ForegroundColor = ConsoleColor.Gray;
@@ -212,15 +269,8 @@ void TokenAnalize(string[] tokens)
         case "" :
             break;
         case "shutdown" :
-            if (tokens[1] == "now")
-            {
-                Process.Start("poweroff -f");
-            }
-            else
-            {
-                Process.Start("poweroff");
-            }
-            return;
+            Process.Start("poweroff -f");
+            break;
     default:
         Error("Invalid command : " + string.Join(" ", tokens));
         break;
@@ -230,11 +280,11 @@ void TokenAnalize(string[] tokens)
 void Start()
 {
     Console.ForegroundColor = ConsoleColor.Magenta;
-    Console.WriteLine(" ██████╗ ██╗ ██╗  ██████╗ ███████╗\n██╔════╝████████╗██╔═══██╗██╔════╝\n██║     ╚██╔═██╔╝██║   ██║███████╗\n██║     ████████╗██║   ██║╚════██║\n╚██████╗╚██╔═██╔╝╚██████╔╝███████║\n ╚═════╝ ╚═╝ ╚═╝  ╚═════╝ ╚══════╝");
+    Console.WriteLine("  ██████╗ ██╗ ██╗  ██████╗ ███████╗\n ██╔════╝████████╗██╔═══██╗██╔════╝\n ██║     ╚██╔═██╔╝██║   ██║███████╗\n ██║     ████████╗██║   ██║╚════██║\n ╚██████╗╚██╔═██╔╝╚██████╔╝███████║\n  ╚═════╝ ╚═╝ ╚═╝  ╚═════╝ ╚══════╝");
     Console.ForegroundColor = ConsoleColor.DarkMagenta;
     Console.WriteLine(" ┌─────────────┬─────────────────────────────────────────┐");
     Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine(" │  Version    │ 1.3                                     │");
+    Console.WriteLine(" │  Version    │ 1.4                                     │");
     Console.Write($" │  User Name  │ {Environment.UserName}");
     for (int i = 0; i < 40 - Environment.UserName.Length; i++)
     {
